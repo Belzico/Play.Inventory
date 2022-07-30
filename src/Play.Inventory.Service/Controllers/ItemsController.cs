a5305e7727559cc09152060971d901ee2b31a6ed
Belzico <namichurri@gmail.com>
2022-07-30 19:31:26 +0200

    nugget error

---
+++ src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,72 @@
+using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Play.Inventory.Service.Dtos;
+using System.Linq;
+using Play.Inventory.Service.Clients;
+
+namespace Play.Inventory.Service.Controllers{
+
+    [ApiController]
+    [Route("items")]
+
+    public class ItemsController : ControllerBase{
+
+        private readonly IRepository<InventoryItem> itemsRepository;
+        private readonly CatalogClient catalogClient;
+    
+    public ItemsController(IRepository<InventoryItem> itemsRepository, CatalogClient catalogClient){
+        this.itemsRepository = itemsRepository;
+        this.catalogClient = catalogClient;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InventoryItemDto>>>  GetAsync(Guid userId){
+        if (userId == Guid.Empty){
+            return BadRequest(); 
+        }
+        //var items = (await itemsRepository.GetAllAsync(item=> item.UserId==userId))
+        //            .Select(item => item.AsDto());
+        var catalogItems = await catalogClient.GetCatalogItemsAsync();
+        var inventoryItemEntities = await itemsRepository.GetAllAsync(item => item.UserId ==userId);
+
+        var inventoryItemDtos=inventoryItemEntities.Select(inventoryItem=>{
+        
+            var catalogItem =catalogItems.Single(catalogItem=> catalogItem.Id==inventoryItem.CatalogItemId);
+            return inventoryItem.AsDto(catalogItem.Name,catalogItem.Description); 
+        }); 
+
+        return Ok(inventoryItemDtos);
+
+
+    }
+    [HttpPost]
+
+    public async Task<ActionResult> PostAsync(GrantItemsDto grantItemsDto){
+        var inventoryItem = await itemsRepository.GetAsync(
+            item => item.UserId==grantItemsDto.UserId && item.CatalogItemId==grantItemsDto.CatalogItemId);
+
+        if (inventoryItem==null){
+            inventoryItem=new InventoryItem{
+                CatalogItemId=grantItemsDto.CatalogItemId,
+                UserId=grantItemsDto.UserId,
+                Quantity=grantItemsDto.Quantity,
+                AcquireDate=DateTimeOffset.UtcNow
+
+            };
+
+            await itemsRepository.CreateAsync(inventoryItem);
+        }
+        else{
+            inventoryItem.Quantity+=grantItemsDto.Quantity;
+            await itemsRepository.UpdateAsync(inventoryItem);
+        }
+        return Ok();
+    }
+
+    }
+}
